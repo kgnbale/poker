b4152be52c9669c743250cdf7863db45d9424a14
Collin <collin@nb.cx>
2018-11-22 15:43:18 +0800

    test

---
+++ server/application/controller/Call.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the NB Framework package.
+ *
+ * Copyright (c) 2018 https://nb.cx All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace controller;
+/**
+ * Call
+ *
+ * @package controller
+ * @link https://nb.cx
+ * @author: collin <collin@nb.cx>
+ * @date: 2018/11/22
+ */
+class Call {
+
+    public function index() {
+        echo 'OK';
+    }
+
+}
